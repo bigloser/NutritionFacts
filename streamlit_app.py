9c48e758c6d56c921be6a9888dcdb8d4dbcf223d
João Cassis <joaopcassis@gmail.com>
2021-04-25 21:31:52 +0100

    First commit

---
+++ streamlit_app.py
@@ -0,0 +1,69 @@
+import pickle
+import re
+import streamlit as st
+import SessionState
+import utils
+
+
+session_state = SessionState.get(session='')
+
+
+st.title('NutritionFacts.Org Live Q&A Browser')
+
+st.markdown(
+        """
+        This tool allows for a quick search for a specific term\
+        on all Live Q&As ever recorded.
+
+        Source: [NutritionFacts](https://nutritionfacts.org/live/)
+        """
+        )
+
+with open('file.pkl', 'rb') as saved_file:
+    saved_file_data = pickle.load(saved_file)
+
+if not session_state.session:
+    saved_file_data = utils.check_new_videos(saved_file_data)
+    session_state.session = True
+
+query = st.text_input('Enter your search term', '')
+if len(query) < 1:
+    st.stop()
+
+search_results = {}
+n = 0
+for video in saved_file_data:
+    if re.findall(query, video[2], flags=re.IGNORECASE):
+        list_of_start_times = []
+        link = f'https://www.youtube.com/watch?v={video[0]}'
+        for caption in video[3]:
+            if re.search(query, caption['text'], flags=re.IGNORECASE):
+                start_time = caption['start']
+                list_of_start_times.append(start_time)
+        for i in range(len(list_of_start_times) - 1, 0, -1):
+            if list_of_start_times[i] - list_of_start_times[i - 1] < 20:
+                del list_of_start_times[i]
+        search_results[n] = {
+                'title': video[1],
+                'link': link,
+                'occurrences': list_of_start_times}
+        n += 1
+if n == 0:
+    st.write("Nothing found")
+    st.stop()
+
+selected_video = st.selectbox(
+        'Select your video',
+        [search_results[i]['title'] for i in search_results.keys()])
+
+for k, v in search_results.items():
+    if selected_video == v['title']:
+        if len(v['occurrences']) > 1:
+            occurrence = st.selectbox(
+                    'Select occurence:',
+                    [i for i in range(len(v['occurrences']))], index=0)
+            st.video(v['link'], start_time=int(v['occurrences'][occurrence]))
+        else:
+            st.video(v['link'], start_time=int(v['occurrences'][0]))
+
+
---
+++ utils.py
@@ -0,0 +1,84 @@
+import re
+import pickle
+import urllib.request
+import datetime
+import bs4 as bs
+import streamlit as st
+from youtube_transcript_api import YouTubeTranscriptApi
+from googleapiclient.discovery import build
+
+api_key = 'AIzaSyC1hwkrmvk10XgATCsaBxXPBx5PvZgmP1g'
+
+
+@st.cache
+def get_new_video_data(set_new_videos):
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    new_video_data = []
+    for video_id in set_new_videos:
+        request = youtube.videos().list(part='snippet', id=video_id)
+        result = request.execute()
+
+        title = result['items'][0]['snippet']['title']
+        date_published = result['items'][0]['snippet']['publishedAt']
+        datetime_obj = datetime.datetime.strptime(
+                date_published, '%Y-%m-%dT%H:%M:%SZ')
+        date_str = datetime_obj.strftime('%B %Y')
+
+        video_transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        text = " ".join(i['text'] for i in video_transcript)
+        new_video_data.append(
+                [video_id, f'{title} {date_str}', text, video_transcript])
+    return new_video_data
+
+
+def get_list_valid_yt_id(html_soup_obj):
+    pattern = r'you.*(watch\?v=)?([a-zA-Z_0-9-]{11})'
+    list_valid_yt_id = []
+    tags = html_soup_obj.find_all('a')
+    for tag in tags:
+        link = tag.get('href')
+        valid_yt_url = re.search(pattern, link)
+        if valid_yt_url:
+            list_valid_yt_id.append(valid_yt_url.group(2))
+    return list_valid_yt_id
+
+
+@st.cache
+def check_new_videos(saved_file_data):
+    old_videos_url = (
+            'https://web.archive.org/web/20200729153409/'
+            'nutritionfacts.org/live')
+    try:
+        old_videos = urllib.request.urlopen(old_videos_url).read()
+        current_videos = urllib.request.urlopen(
+            'https://nutritionfacts.org/live/').read()
+    except Exception:
+        pass
+    else:
+        old_videos_html = bs.BeautifulSoup(
+                old_videos,
+                'html.parser',
+                from_encoding='utf-8')
+        current_videos_html = bs.BeautifulSoup(
+                current_videos,
+                'html.parser',
+                from_encoding='utf-8')
+
+        list_valid_yt_id = []
+
+        current_videos_valid_yt_id = get_list_valid_yt_id(current_videos_html)
+        old_videos_valid_yt_id = get_list_valid_yt_id(old_videos_html)
+
+        list_valid_yt_id = current_videos_valid_yt_id + old_videos_valid_yt_id
+        list_valid_yt_id = set(list_valid_yt_id)
+        list_valid_yt_id.remove('ionFactsOrg')
+
+        set_new_videos = list_valid_yt_id - set(
+                [i[0] for i in saved_file_data])
+        if set_new_videos:
+            new_video_data = get_new_video_data(set_new_videos)
+            saved_file_data = saved_file_data + new_video_data
+            with open('file.pkl', 'wb') as handle:
+                pickle.dump(saved_file_data, handle, protocol=4)
+
+    return saved_file_data
