87aeb93a2b93e44c549cbf041ea23783639dc7b7
João Cassis <joaopcassis@gmail.com>
2021-04-27 13:14:15 +0100

    Trying to fix analytics

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import re
 import streamlit as st
 import utils
 import streamlit_analytics
+import SessionState
 
 
-session_state = streamlit_analytics.sess.get(session='')
+session_state = SessionState.get(session='', last_time='')
 
 st.title('NutritionFacts.Org Live Q&A Browser')
 
